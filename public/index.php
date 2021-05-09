39e77e3c12cefd22b3fa7ef41022b3f870a66323
Dominik Gwozdz <dwgwozdz@gmail.com>
2021-05-09 16:26:18 +0200

    handle request with basic files

--- public/index.php
+++ public/index.php
@@ -2,3 +2,4 @@
 
 require '../vendor/autoload.php';
 
+echo \App\Home\Home::index();
---
+++ src/Home/Home.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Home;
+
+class Home
+{
+    /**
+     * @return string
+     */
+    public static function index(): string
+    {
+        return 'Hello my friend. It works!';
+    }
+}
