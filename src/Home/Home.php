4f93f5d85a8b42c0682d8d2619aeceac381d3d78
Dominik <dwgwozdz@gmail.com>
2022-08-13 21:30:47 +0200

    Update PHP, add Makefile to enter container shell easily

--- src/Home/Home.php
+++ src/Home/Home.php
@@ -1,12 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Home;
 
 class Home
 {
-    /**
-     * @return string
-     */
     public static function index(): string
     {
         return 'Hello my friend. It works!';
